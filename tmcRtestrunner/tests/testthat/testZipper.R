d00e270c8a97915c73bab701478b4ae9c4de10e3
Aleksis Vuoksenmaa <aleksis.vuoksenmaa@helsinki.fi>
2017-10-15 18:52:20 +0300

    Added zipping and unzipping + tar functionality

---
+++ tmcRtestrunner/tests/testthat/testZipper.R
@@ -0,0 +1,23 @@
+test_resources_dir <- paste(sep = "", getwd(), "/resources")
+
+pack_folder_path <- paste(sep = "", test_resources_dir, "/pack")
+
+
+test_that("Zipfile is created", {
+  .tmc_zip(pack_folder_path, "resources/testZip")
+  expect_true(file.exists("resources/testZip.zip"))
+  file.remove("resources/testZip.zip")
+})
+
+test_that("Tarfile is created", {
+  .tmc_tar(pack_folder_path, "resources/testTar")
+  expect_true(file.exists("resources/testTar.tar"))
+  file.remove("resources/testTar.tar")
+})
+
+test_that("Zipfile is unpacked", {
+  .tmc_zip(pack_folder_path, "resources/testZip")
+  .tmc_unzip("resources/testZip.zip", "resources/unpack")
+  file.remove("resources/testZip.zip")
+  expect_true(file.exists("resources/unpack/zipfile1.R"))
+})
